f5ba15d8c7c2f80351f7b6100bd72f066f77b014
Pedro Arruda <pedro.arruda@allcomsistemas.com.br>
2024-08-02 13:42:20 -0300

    feat: add button to upload data

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -9,7 +9,7 @@ export const Button = ({ children, className, ...rest }: IButton) => {
   return (
     <button
       {...rest}
-      className={`bg-[#ACF463]  text-neutral-700 font-semibold cursor-pointer flex items-center justify-between gap-3 ${className}`}
+      className={`bg-[#ACF463]  text-neutral-700 font-semibold cursor-pointer flex items-center justify-center gap-3 ${className}`}
     >
       {children}
     </button>
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,6 +1,30 @@
 import { ChartLine, CurrencyCircleDollar } from "phosphor-react";
+import { useState } from "react";
+import { useAuth } from "../contexts/PlugglyContext";
+import { fetchApi } from "../functions/fetchApi";
+import { Button } from "./Button";
 
 export const Sidebar = () => {
+  const { auth } = useAuth();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleUpdateItem = async () => {
+    if (auth) {
+      setIsLoading(true);
+      try {
+        await fetchApi("/item", "POST", {
+          itemId: auth.itemId,
+        });
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+  };
+
+  console.log(isLoading);
+
   return (
     <div className="flex flex-col items-center max-w-96 min-w-[250px] bg-[#1d1d21] p-10 h-screen text-neutral-300">
       <h1 className="text-2xl font-semibold">LOGO</h1>
@@ -18,6 +42,15 @@ export const Sidebar = () => {
           </a>
         </li>
       </ul>
+      <div className="mt-10 w-full">
+        <Button
+          className="py-1 w-full"
+          onClick={() => handleUpdateItem()}
+          disabled={isLoading}
+        >
+          {!isLoading ? "Refresh data" : "Refreshing..."}
+        </Button>
+      </div>
     </div>
   );
 };
